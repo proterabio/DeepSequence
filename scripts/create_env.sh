f80dff21df7e0f2b51c289585cf73a70d9d39af7
Michael Dunne <mdunne@proterabio.com>
2022-06-23 09:47:22 +0100

    Scripts to create the environment

---
+++ scripts/create_env.sh
@@ -0,0 +1,9 @@
+conda create -n deepsequence2 python=2.7 anaconda
+conda activate deepsequence2
+
+conda install mkl-service;
+conda install -c conda-forge pygpu;
+conda install mako;
+pip uninstall numpy;
+pip uninstall numpy;
+pip install numpy
